e8e65c39e893595cdece8313c1a01b562d373473
Kevin Jalbert <kevin.j.jalbert@gmail.com>
2019-06-23 15:29:22 -0400

    feat: add ability to add tasks

--- src/api.py
+++ src/api.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S PATH="${PATH}:/usr/local/bin" python3
 
-from notion_api import appendToCurrentDayNotes
+from notion_api import appendToCurrentDayNotes, tasksDatabase
 
 from flask import Flask, request
 app = Flask(__name__)
@@ -10,10 +10,28 @@ app = Flask(__name__)
 def add_note():
     try:
         note = request.args.get('title')
+
         appendToCurrentDayNotes(note)
+
         return 'Succeceed in adding note', 200
     except Exception:
         return 'Failed in adding note', 500
+
+
+@app.route('/add_task')
+def add_task():
+    try:
+        task = request.args.get('title')
+
+        collection = tasksDatabase().collection
+        row = collection.add_row()
+        row.name = task
+        row.status = 'Next Up'
+        row.tags = ['Imported']
+
+        return 'Succeceed in adding task', 200
+    except Exception:
+        return 'Failed in adding task', 500
 
 
 if __name__ == '__main__':
