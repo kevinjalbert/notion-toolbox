26d3712a2869b699cbcbf7691807dc9b2a5bc4dd
Kevin Jalbert <kevin.j.jalbert@gmail.com>
2019-08-18 15:15:30 -0400

    refactor: duplicated code out into new repository/package notion-scripts
    To share similar code between this project and alfred-notion (along with
    other future projects) a new repository notion-scripts was created.
    
    This commit removes the duplicated code and uses the new package. The
    config file isn't needed anymore, as the default config in
    notion-scripts is the same as what this project needed.
    
    Some refactoring was done in the new package to be more class-based.
    
    Some additional refactors were done to better accomodate the new
    changes.
    
    The transition_tasks scheduled job is using the same behaviour of
    creating a new notion client for each iteration.

--- src/api.py
+++ src/api.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env -S PATH="${PATH}:/usr/local/bin" python3
 
-from notion_api import appendToCurrentDayNotes, tasksDatabase
-from config import importedTagURL
+from notionscripts.notion_api import NotionApi
 
 from flask import Flask, request
 from flask_apscheduler import APScheduler
 app = Flask(__name__)
 
 
-class Config(object):
+class JobConfig(object):
     JOBS = [
         {
             'id': 'add_task_transitions',
-            'func': 'update_task_transitions:add_task_transitions',
+            'func': 'api:transition_tasks',
             'args': (),
             'trigger': 'interval',
             'seconds': 60
@@ -29,12 +28,18 @@ class Config(object):
     SCHEDULER_API_ENABLED = True
 
 
+def transition_tasks():
+    NotionApi().transition_tasks()
+
+
 @app.route('/add_note')
 def add_note():
     try:
+        notion_api = NotionApi()
+
         note = request.args.get('title')
 
-        appendToCurrentDayNotes(note)
+        notion_api.append_to_current_day_notes(note)
 
         return 'Succeceed in adding note', 200
     except Exception:
@@ -44,20 +49,21 @@ def add_note():
 @app.route('/add_task')
 def add_task():
     try:
+        notion_api = NotionApi()
         task = request.args.get('title')
 
-        collection = tasksDatabase().collection
+        collection = notion_api.tasks_database().collection
         row = collection.add_row()
         row.name = task
         row.status = 'Next Up'
-        row.tags = [importedTagURL()]
+        row.tags = [notion_api.config.imported_tag_url()]
 
         return 'Succeceed in adding task', 200
     except Exception:
         return 'Failed in adding task', 500
 
 
-app.config.from_object(Config())
+app.config.from_object(JobConfig())
 
 scheduler = APScheduler()
 scheduler.init_app(app)
