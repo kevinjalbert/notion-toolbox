2dc1c729cc605839f4003d565e2257e92f4b88f9
Kevin Jalbert <kevin.j.jalbert@gmail.com>
2019-08-29 21:53:55 -0400

    fix: update version to correct numbering

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='notion-scripts',
-      version='0.1',
+      version='0.1.0',
       description='Shared collection of common Notion Scripts used in my projects',
       url='http://github.com/kevinjalbert/notion-scripts',
       author='Kevin Jalbert',
