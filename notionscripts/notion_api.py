9284403729b241a4379faa88811f4d1aa555c3e5
Kevin Jalbert <kevin.j.jalbert@gmail.com>
2019-09-09 20:33:31 -0400

    feat: add ability to fetch all current tasks
    Add ability to fetch all the current tasks in the task database. Using
    a specific query to fetch the exact tasks.

--- notionscripts/notion_api.py
+++ notionscripts/notion_api.py
@@ -84,5 +84,16 @@ class NotionApi():
 
         return note_block
 
+    def get_current_tasks(self):
+        filter_params = [
+          {
+            "property": "status",
+            "comparator": "enum_is",
+            "value": "Current"
+          }
+        ]
+        current_tasks_query = self.tasks_database().build_query(filter=filter_params)
+        return current_tasks_query.execute()
+
     def transition_tasks(self):
         TasksTransitionUpdater(self).process()
