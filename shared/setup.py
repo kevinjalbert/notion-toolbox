d311336f751106db1bd0c14a87f0a9982dbcbd40
Kevin Jalbert <kevin.j.jalbert@gmail.com>
2019-12-27 19:02:14 -0500

    feat: make alfred and server install shared with editable mode
    This change makes it much easier to work with the tools. Changes made
    in the shared package is reflected in the other tools without any need
    to reinstall.
    
    Dropping the version on the shared notion-scripts as the git sha
    suffices now.

--- shared/setup.py
+++ shared/setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup
 
 setup(name='notion-scripts',
-      version='0.2.1',
       description='Shared collection of common Notion Scripts used in my projects',
       url='http://github.com/kevinjalbert/notion-scripts',
       author='Kevin Jalbert',
