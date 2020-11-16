6324280505f153a9fd9a506474924dfce23d2eec
Kevin Jalbert <kevin.j.jalbert@gmail.com>
2020-11-15 20:48:22 -0500

    fix: some blocks don't have a title like the DividerBlock
    This makes it so they are handled correctly when displayed via the API.

--- shared/notionscripts/block_presenter.py
+++ shared/notionscripts/block_presenter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env -S PATH="${PATH}:/usr/local/bin" python3
 
 from notion.collection import CollectionRowBlock, CollectionView
+from notion.block import DividerBlock
 
 
 class BlockPresenter(dict):
@@ -12,5 +13,7 @@ class BlockPresenter(dict):
         elif isinstance(block, CollectionView):
             dict.__init__(self, **{"collection_id": block.parent.id.replace("-", ""), "view_id": block.id.replace("-", ""),
                                    "collection_title": block.parent.title, "view_title": block.name})
+        elif isinstance(block, DividerBlock):
+            dict.__init__(self, **{"id": block.id.replace("-", "")})
         else:
             dict.__init__(self, **{"id": block.id.replace("-", ""), "title": block.title})
